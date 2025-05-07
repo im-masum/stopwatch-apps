a6b1bbd3db50748ee9c94f80dee73fad6e826ab2
MIchowdhury <masum.iqbal.chowdhury@gmail.com>
2025-05-08 03:23:58 +0600

    I have made some changes to some files.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let resetBtn = document.getElementById("resetBtn");
 let msec = 0;
 let secs = 0;
 let mins = 0;
+let hrs = 0;
 
 let timerId = null;
 
@@ -22,7 +23,7 @@ stopBtn.addEventListener("click", function () {
 
 resetBtn.addEventListener("click", function () {
   clearInterval(timerId);
-  timerDisplay.innerHTML = `00 : 00 : 00`;
+  timerDisplay.innerHTML = ` 00 : 00 : 00`;
   msec = secs = mins = 0;
 });
 
@@ -31,15 +32,20 @@ function startTimer() {
   if (msec == 100) {
     msec = 0;
     secs++;
-    if ((secs = 60)) {
+    if (secs == 60) {
       secs = 0;
       mins++;
+    }
+    if (mins == 60) {
+      mins = 0;
+      hrs++;
     }
   }
 
   let msecString = msec < 10 ? `0${msec}` : msec;
   let secsString = secs < 10 ? `0${secs}` : secs;
-  let minsString = mins < 10 ? `0${mins}` : mins;             
+  let minsString = mins < 10 ? `0${mins}` : mins;
+  let hrsString = hrs < 10 ? `0${hrs}` : hrs;
 
-  timerDisplay.innerHTML = `${secsString} : ${minsString} : ${msecString}`;
+  timerDisplay.innerHTML = `${hrsString} : ${minsString} : ${secsString} : ${msecString}`;
 }
